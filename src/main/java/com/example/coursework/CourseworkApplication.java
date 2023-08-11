67721d56c7913e9ab8a0107aa1698463977955fd
madahtoxa <madahtoxa@mail.ru>
2023-08-11 12:32:00 +0300

    Initial commit

---
+++ src/main/java/com/example/coursework/CourseworkApplication.java
@@ -0,0 +1,14 @@
+package com.example.coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseworkApplication.class, args);
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/com/example/coursework/CourseworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello ");
+
+    }
+
+
+}
